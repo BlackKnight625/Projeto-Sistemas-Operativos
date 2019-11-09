2741af4461ef432faaf6e97228e75c996ca834b1
BlackKnight625 <rafaellourenco2011@hotmail.com>
2019-11-09 23:25:12 +0000

    Script is more beautiful

--- runTests.sh
+++ runTests.sh
@@ -6,7 +6,7 @@ numbuckets=$4
 for f in $(ls $inputdir)
 do
     inputfile=$inputdir/$f
-    outputfile=$outputdir/$f-1.txt
+    outputfile=$outputdir/${f%.*}-1.txt
     echo Inputfile=$inputfile Numthreads=1
     ./tecnicofs-nosync $inputfile $outputfile 1 1 | grep TecnicoFs
 done
@@ -15,7 +15,7 @@ do
     for numthreads in $(seq 2 $maxthreads)
     do
         inputfile=$inputdir/$f
-        outputfile=$outputdir/$f-$numthreads.txt
+        outputfile=$outputdir/${f%.*}-$numthreads.txt
         echo Inputfile=$inputfile Numthreads=$numthreads
         ./tecnicofs-mutex $inputfile $outputfile $numthreads $numbuckets | grep TecnicoFs
     done
