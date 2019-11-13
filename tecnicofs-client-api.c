461bb8bfd7b538d8b339e554f500d4772d0684ba
BlackKnight625 <rafaellourenco2011@hotmail.com>
2019-11-13 15:44:54 +0000

    Added Execise 3 code and created tecnicofs-client-api.c

---
+++ tecnicofs-client-api.c
@@ -0,0 +1,29 @@
+#include "tecnicofs-api-constants.h"
+
+int tfsCreate(char *filename, permission ownerPermissions, permission othersPermissions) {
+
+}
+int tfsDelete(char *filename) {
+
+}
+int tfsRename(char *filenameOld, char *filenameNew) {
+
+}
+int tfsOpen(char *filename, permission mode) {
+
+}
+int tfsClose(int fd) {
+
+}
+int tfsRead(int fd, char *buffer, int len) {
+
+}
+int tfsWrite(int fd, char *buffer, int len) {
+
+}
+int tfsMount(char * address) {
+
+}
+int tfsUnmount() {
+    
+}
